6f80923c8656f0236776089e6061c6c1c281a1fe
KhaledMAlasmari <sirkhaled7@gmail.com>
2022-04-26 10:36:46 +0300

    Apparently im retarded at python oop

--- Objects/Course.py
+++ Objects/Course.py
@@ -1,8 +1,6 @@
 from .Section import Section
 
 class Course:
-    __sections = None
-    __course_name = None
     def __init__(self, sections: list[Section], course_name: str) -> None:
         self.__sections = sections
         self.__course_name = course_name
