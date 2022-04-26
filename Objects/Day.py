c22479a7b33fc2f5377e3a73ba0726db51a6fe3b
KhaledMAlasmari <sirkhaled7@gmail.com>
2022-04-26 10:32:41 +0300

    Day objects saves lectures instead of sections

--- Objects/Day.py
+++ Objects/Day.py
@@ -1,60 +1,50 @@
 from datetime import date, datetime, time
+from Objects.Lecture import Lecture
 
 from Objects.Section import Section
 
 
 class Day:
-    __classes: list[Section] = []
-    __earliest_time = None
 
-    def __init__(self, earliest_time: time) -> None:
-        self.__earliest_time = earliest_time
+    def __init__(self) -> None:
+        self.__lectures: list[Lecture] = []
 
-    def get_classes(self):
-        self.__earliest_time = 123
-        return self.__classes
+    def get_lectures(self):
+        return self.__lectures
 
-    def get_earliest_time(self):
-        return self.__earliest_time
-
-    def is_conflict(self, section: Section):
-        length = len(self.__classes)
-        new_lecture_start_time = section.get_normal_lecture_start_time()
-        new_lecture_end_time = section.get_normal_lecture_end_time()
+    def is_conflict(self, new_lecture_start_time: date, new_lecture_end_time: date):
+        length = len(self.__lectures)
         for i in range(length):
-            current_lecture_start_time = self.__classes[i].get_normal_lecture_start_time()
-            current_lecture_end_time = self.__classes[i].get_normal_lecture_end_time()
+            current_lecture_start_time = self.__lectures[i].start_time
+            current_lecture_end_time = self.__lectures[i].end_time
+            # check if normal lecture is conflict
             if (new_lecture_start_time < current_lecture_end_time) and (new_lecture_end_time > current_lecture_start_time):
                 return True
         return False
-    def add_section(self, section):
-        self.__classes.append(section)
+
+    def add_lecture(self, lecture: Lecture):
+        self.__lectures.append(lecture)
 class Sunday(Day):
-    def __init__(self, earliest_time) -> None:
-        self.get_classes()
-        super().__init__(earliest_time)
+    def __init__(self, ) -> None:
+        super().__init__()
 
 class Monday(Day):
-    def __init__(self, earliest_time) -> None:
-        self.get_classes()
-        super().__init__(earliest_time)
+    def __init__(self, ) -> None:
+        super().__init__()
 
 
 class Tuesday(Day):
-    def __init__(self, earliest_time) -> None:
-        self.get_classes()
-        super().__init__(earliest_time)
+    def __init__(self, ) -> None:
+        super().__init__()
 
 
 class Wednesday(Day):
-    def __init__(self, earliest_time) -> None:
-        self.get_classes()
-        super().__init__(earliest_time)
+    def __init__(self, ) -> None:
+        super().__init__()
 
 
 class Thursday(Day):
-    def __init__(self, earliest_time) -> None:
-        self.get_classes()
-        super().__init__(earliest_time)
+    def __init__(self, ) -> None:
+        super().__init__()
         
     
